9293fd23bda7cf3059a21b4a2d5e4032a869ae8a
gosuexp <gosuexp@gmail.com>
2022-04-09 22:14:23 +0300

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +14,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 from app import routes, models
