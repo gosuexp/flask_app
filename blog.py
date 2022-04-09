25a725cba73421b662fef29f338dbf988ad00202
gosuexp <gosuexp@gmail.com>
2022-04-09 14:27:17 +0300

    followers db

--- blog.py
+++ blog.py
@@ -1,3 +1,4 @@
+
 from app import app,db
 from app.models import User, Post
 
@@ -5,3 +6,5 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+
---
+++ migrations/versions/4713c049842f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 4713c049842f
+Revises: bb3de1a402be
+Create Date: 2022-04-09 14:11:15.992144
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4713c049842f'
+down_revision = 'bb3de1a402be'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
